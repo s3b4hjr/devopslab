d80c357061415e9a00dd6dae8ed1104b7ec17344
Sebastiao Junior <65188546+s3b4hjr@users.noreply.github.com>
2022-05-21 10:46:33 -0300

    messa error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Devops Lab Impacta Sebah!!"
 
 if __name__ == '__main__':
     app.run()
